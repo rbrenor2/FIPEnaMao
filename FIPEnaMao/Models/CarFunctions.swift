c329339906da50d977bcc01bb54c3a1f04824532
brenor2 <rbrenorios@gmail.com>
2018-05-26 10:44:14 -0300

    Initial Commit

---
+++ FIPEnaMao/Models/CarFunctions.swift
@@ -0,0 +1,28 @@
+//
+//  CarFunctions.swift
+//  FIPEnaMao
+//
+//  Created by Breno Ramos on 26/05/18.
+//  Copyright © 2018 brenor2. All rights reserved.
+//
+
+import Foundation
+
+class CarFunctions {
+    //static let's you use functions without instantiaing the class
+    static func createCar(carModel: CarModel) {
+        
+    }
+    
+    static func readCars() {
+        
+    }
+    
+    static func updateCar(carModel: CarModel) {
+        
+    }
+    
+    static func deleteCar(carModel: CarModel) {
+        
+    }
+}
---
+++ FIPEnaMao/Models/CarModel.swift
@@ -0,0 +1,23 @@
+//
+//  CarModel.swift
+//  FIPEnaMao
+//
+//  Created by Breno Ramos on 26/05/18.
+//  Copyright © 2018 brenor2. All rights reserved.
+//
+
+import Foundation
+
+class CarModel {
+    let id   : String!
+    let brand: String!
+    let model: String!
+    let year : String!
+    
+    init(brand: String!, model: String!, year: String!) {
+        id = UUID().uuidString
+        self.brand = brand
+        self.model = model
+        self.year  = year
+    }
+}
