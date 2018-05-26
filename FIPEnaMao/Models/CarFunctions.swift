b545b27ae391c4e19fb33eb780f11f312f78cf0a
brenor2 <rbrenorios@gmail.com>
2018-05-26 10:51:45 -0300

    Added readCars code.

--- FIPEnaMao/Models/CarFunctions.swift
+++ FIPEnaMao/Models/CarFunctions.swift
@@ -15,7 +15,15 @@ class CarFunctions {
     }
     
     static func readCars() {
-        
+        if Data.tripModels.count == 0 {
+            Data.tripModels.append(CarModel(brand: "Peugeot", model: "RCZ", year: "2015 Gasolina"))
+            Data.tripModels.append(CarModel(brand: "BMW", model: "i3", year: "2018 Elétrico"))
+            Data.tripModels.append(CarModel(brand: "Audi", model: "A4", year: "2015 Gasolina"))
+            Data.tripModels.append(CarModel(brand: "Volkswagen", model: "RCZ", year: "2015 Gasolina"))
+            Data.tripModels.append(CarModel(brand: "Fiat", model: "RCZ", year: "2015 Gasolina"))
+            Data.tripModels.append(CarModel(brand: "Renault", model: "RCZ", year: "2015 Gasolina"))
+            Data.tripModels.append(CarModel(brand: "Peugeot", model: "207", year: "2010 Flex"))
+        }
     }
     
     static func updateCar(carModel: CarModel) {
---
+++ FIPEnaMao/Models/Data.swift
@@ -0,0 +1,13 @@
+//
+//  Data.swift
+//  FIPEnaMao
+//
+//  Created by Breno Ramos on 26/05/18.
+//  Copyright © 2018 brenor2. All rights reserved.
+//
+
+import Foundation
+
+class Data {
+    static var tripModels = [CarModel]()
+}
